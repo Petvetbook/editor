d9b73d52e42da1acbf250583d3abf0a4eb7f328d
Palash M <mpalash@gmail.com>
2016-05-13 12:46:56 +0300

    Init styles

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,31 @@ var realm = require('realm-js');
 var runSequence = require('run-sequence');
 var uglify = require('gulp-uglify');
 var addsrc = require('gulp-add-src');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
 
 var spawn = require('child_process').spawn;
 var node;
+
+gulp.task('sass', function() {
+   return sass('src/scss/styles.scss', {
+         sourcemap: true
+      }).on('error', function(err) {
+         console.error('Error!', err.message);
+      })
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions', 'ie 8', 'ie 9'],
+         map: {
+            inline: false
+         }
+      }))
+      .pipe(cssmin({
+         sourceMap: true
+      }))
+      .pipe(rename('editor.min.css'))
+      .pipe(gulp.dest('build/'));
+});
 
 gulp.task('watch', function() {
    gulp.watch(['src/**/*.js'], ['build']);
@@ -28,8 +50,8 @@ gulp.task('server', function() {
 });
 
 gulp.task('start', ['server'], function() {
-   gulp.watch(['src/**/*.js'], function() {
-      runSequence('build', 'server')
+   gulp.watch(['src/**/*.js','src/scss/*'], function() {
+      runSequence('build', 'sass', 'server')
    });
 });
 
