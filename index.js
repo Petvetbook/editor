225aedd24c43916387cc172dd7b05ff1cf11118c
Ivan.Orlov <ivan@morrr.com>
2016-05-24 12:15:06 +0300

    index.js changed

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
 var realm = require('realm-js');
 var fs = require('fs');
 
-var jsMinifiedFile = __dirname + "/dist/editor.min.js";
-var jsRaw = __dirname + "/dist/editor.js";
-var cssFile = __dirname + "/dist/editor.min.css";
-require(jsRaw);
-
-var js_contents, css_contents;
-module.exports = {
-   js: function(req, res, next) {
-      var self = this;
-      return function(req, res, next) {
-         res.setHeader('content-type', 'text/javascript');
-         return res.end(self.getJS(false));
-      }
-   },
-   css: function(req, res, next) {
-      var self = this;
-      return function(req, res, next) {
-         res.setHeader('content-type', 'text/css');
-         return res.end(self.getCSS());
-      }
-   },
-   getJS: function(minified) {
-      js_contents = js_contents || fs.readFileSync(minified ? jsMinifiedFile : jsRaw).toString();
-      return js_contents;
-   },
-   getCSS: function() {
-      css_contents = css_contents || fs.readFileSync(cssFile).toString();
-      return css_contents;
-   }
-}
+require(__dirname + "/dist/backend/backend.js")
